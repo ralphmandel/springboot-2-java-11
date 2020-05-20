bb2bf1c999f581b6723d37f84532515e4f05822a
ralphmandel <ralphmandelbarros@gmail.com>
2020-05-20 18:40:08 -0300

    User entity and resource

---
+++ src/main/java/com/ralphmandel/sts/resources/UserResource.java
@@ -0,0 +1,24 @@
+package com.ralphmandel.sts.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ralphmandel.sts.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Ralph", "ralph@gmail.com", "99999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+	
+	
+	
+	
+	
+}
