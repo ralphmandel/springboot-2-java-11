11a9612aae34792eb14e694a8b145c028fedc42b
ralphmandel <ralphmandelbarros@gmail.com>
2020-05-20 18:18:15 -0300

    Project created

---
+++ src/main/java/com/ralphmandel/sts/StsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ralphmandel.sts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ralphmandel/sts/StsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ralphmandel.sts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
