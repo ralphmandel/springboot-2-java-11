9b2b319f340429ef596f91f69cf8b105972de027
ralphmandel <ralphmandelbarros@gmail.com>
2020-05-21 11:55:34 -0300

    Service layer, component registration

---
+++ src/main/java/com/ralphmandel/sts/services/UserService.java
@@ -0,0 +1,27 @@
+package com.ralphmandel.sts.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ralphmandel.sts.entities.User;
+import com.ralphmandel.sts.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findbyId(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
